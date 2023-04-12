1b77031ed64e5dd46f102a4b82874fd9acd098db
Vikitohyah <victoriaedorh@gmail.com>
2023-04-12 16:20:04 -0700

    Python input & output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,6 +6,7 @@ from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+
 filename = 'add_item.json'
 my_list = []
 try:
