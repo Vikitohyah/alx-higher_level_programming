dc334aec6aaf6ace75819d4d6c7fe5a78ffedab4
Vikitohyah <victoriaedorh@gmail.com>
2023-05-17 11:19:20 -0700

    SQL

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,7 +1,3 @@
--- It lists all rows of the table second_table
--- from the database hbtn_0c_0
--- results display score and name
--- ordered by score
--- with score >= 10
+-- A SQL query that list all databases
 
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SHOW DATABASES;
